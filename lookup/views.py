1c5b578c664897645e4d946f75fb29e7722053e7
sushil518 <bhaleraosushil518@gmail.com>
2021-09-08 18:57:03 +0530

    final commit

--- lookup/views.py
+++ lookup/views.py
@@ -1,7 +1,18 @@
 from django.shortcuts import render
 
 def home(request):
-    return render(request, 'home.html', {})
+    import json
+    import requests
+
+    api_request = requests.get("http://api.airvisual.com/v2/city?city=Pune&state=Maharashtra&country=India&key=a00e5775-2ce6-44a9-a6ed-841255c322d9")
+
+    try:
+        api = json.loads(api_request.content)
+    
+    except Exception as e:
+        api = "Error..."
+
+    return render(request, 'home.html', {'api': api})
 
 def about(request):
     return render(request, 'about.html', {})
